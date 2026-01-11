aced83329ed9aeab19699f25f0dfc8f1413d44b0
Akshata6524 <nagalotiakshata@gmail.com>
2026-01-11 19:37:59 +0530

    Add postfix evaluation functionality
    Implement postfix expression evaluation in C.

---
+++ postfix_evaluation.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<ctype.h>
+#include<string.h>
+
+int compute(char symbol ,int op1 ,int op2){
+    switch(symbol){
+        case '+': return op1+op2;
+        case '-':return op1-op2;
+        case '*':return op1*op2;
+        case '/':return op1/op2;
+        case '%':return op1%op2;
+        case '$':
+        case '^':return pow(op1,op2);
+    }
+}
+
+int main(){
+    int s[20],op1,op2,top=-1,res;
+    char postfix[20],symbol;
+    
+    printf("Enter the postfix expression:");
+    scanf("%s",postfix);
+    
+    for(int i=0;i<strlen(postfix);i++){
+        symbol = postfix[i];
+        if (isdigit(symbol)){
+            s[++top]=symbol-'0';
+        }
+        else{
+            op2=s[top--];
+            op1=s[top--];
+            res=compute(symbol,op1,op2);
+            s[++top]=res;
+        }
+    }
+            res=s[top--];
+            printf("Result=%d",res);
+
+}
